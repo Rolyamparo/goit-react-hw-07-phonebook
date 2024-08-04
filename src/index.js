47e36d15d50b24b1a12327d72db12595026d2e92
Rolyamparo <143687528+Rolyamparo@users.noreply.github.com>
2024-08-04 22:07:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <App />
       </PersistGate>
-    </Provider>
+      </Provider>
   </React.StrictMode>
 );
